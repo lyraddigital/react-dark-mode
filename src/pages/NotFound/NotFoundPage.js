c6f28d995138ecb383a619d01e2c67a3b71ca864
darylduck <daryl.duckmanton@gmail.com>
2021-10-16 12:22:34 +1100

    Fixed Not Found

--- src/pages/NotFound/NotFoundPage.js
+++ src/pages/NotFound/NotFoundPage.js
@@ -4,11 +4,13 @@ import style from './NotFoundPage.module.scss';
 
 export const NotFoundPage = () => {
     return (
-        <Flex justifyContent="center" alignItems="center">
-            <div id={ style.notFoundSection }>
-                <h2>Not Found</h2>
-                <p>We could not find the page you were looking for.</p>
-            </div>
-        </Flex>
+        <div id={ style.notFoundContainer }>
+            <Flex justifyContent="center" alignItems="center">
+                <div id={ style.notFoundSection }>
+                    <h2>Not Found</h2>
+                    <p>We could not find the page you were looking for.</p>
+                </div>
+            </Flex>
+        </div>        
     );
 }
